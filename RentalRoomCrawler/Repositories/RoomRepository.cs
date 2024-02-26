2b0784496153014d514d6da0da0a3252b08d0d07
ducna0610 <ducna0610@gmail.com>
2024-02-26 23:55:54 +0700

    add repositories

---
+++ RentalRoomCrawler/Repositories/RoomRepository.cs
@@ -0,0 +1,39 @@
+﻿using MySql.Data.MySqlClient;
+using RentalRoomCrawler.Models;
+
+namespace RentalRoomCrawler.Repositories;
+
+public class RoomRepository
+{
+
+    /// <summary>
+    /// 
+    /// This class is used for insert data to Database
+    /// 
+    /// </summary>
+    public async Task<bool> InsertRoomAsync(Room? data, MySqlConnection connection)
+    {
+        try
+        {
+            MySqlCommand comm = connection.CreateCommand();
+            comm.CommandText = "INSERT INTO rooms (owner_name, title, description, area, phone, price, address, image, is_active, user_id)"
+                            + "VALUES(@owner_name, @title, @description, @area, @phone, @price, @address, @image, @is_active, @user_id)";
+            comm.Parameters.AddWithValue("@owner_name", data.OwnerName);
+            comm.Parameters.AddWithValue("@title", data.Title);
+            comm.Parameters.AddWithValue("@description", data.Description);
+            comm.Parameters.AddWithValue("@area", data.Area);
+            comm.Parameters.AddWithValue("@phone", data.Phone);
+            comm.Parameters.AddWithValue("@price", data.Price);
+            comm.Parameters.AddWithValue("@address", data.Address);
+            comm.Parameters.AddWithValue("@image", data.Image);
+            comm.Parameters.AddWithValue("@is_active", true);
+            comm.Parameters.AddWithValue("@user_id", 2);
+
+            return comm.ExecuteNonQuery() > 0;
+        }
+        catch (Exception e)
+        {
+            throw;
+        }
+    }
+}
