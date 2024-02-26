090dff0ef23adbbf541517cf203e536b47426ae7
ducna0610 <ducna0610@gmail.com>
2024-02-26 23:34:54 +0700

    add class room model

---
+++ RentalRoomCrawler/Models/Room.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+
+namespace RentalRoomCrawler.Models;
+
+public class Room
+{
+    [Description("Tên chủ trọ")]
+    public string OwnerName { get; set; }
+
+    [Description("Tiêu đề")]
+    public string Title { get; set; }
+
+    [Description("Mô tả")]
+    public string Description { get; set; }
+
+    [Description("Diện tích")]
+    public string Area { get; set; }
+
+    [Description("Số điện thoại")]
+    public string Phone { get; set; }
+
+    [Description("Giá cho thuê")]
+    public string Price { get; set; }
+
+    [Description("Địa chỉ")]
+    public string Address { get; set; }
+
+    [Description("Hình ảnh")]
+    public string Image { get; set; }
+}
